edd32ea0a34d532deb2d3afde274455a255ddeef
Richard Hitier <richard.hitier@co-libri.org>
2015-09-11 09:51:34 +0200

    use url and querystring to parse parameters

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
 var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
 
 function onRequest(req, res) {
-    console.log('requesting');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Bonjour Monde\n');
+
+    var page = url.parse(req.url).pathname;
+    var params = querystring.parse(url.parse(req.url).query);
+    var myRequest='requesting page: '+ page;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    //routing
+    res.write(getHtmlHeader());
+    res.write( myRequest+"</br>");
+    if( 'nom' in params)
+        res.write( "votre nom: "+params['nom']);
+    if (page=='/') {
+        res.write("</br>A la racine");
+    } else if  ( page=='/mapage') {
+        res.write("</br>A ta page");
+    } else {
+        res.write("</br>mauvaise page");
+    }
+    res.write(getHtmlFooter());
+    res.end();
 }
 
+function getHtmlHeader(){
+    return('<!doctype html>'
+            +'<html lang="fr">'
+            +'<head>'
+            +'<meta Charset="utf-8">'
+            +'<title>standalone nodejs</title>'
+            +'</head>'
+            +'<body>'
+            +'<h1>Hey ! look at this title !!!</h1>'
+            +'<p>paragraph</p>');
+
+}
+
+function getHtmlFooter(){
+            return('</body>'
+            +'</html>');
+}
 function start() {
 
-http.createServer( onRequest ).listen(1337, '127.0.0.1');
+http.createServer( onRequest ).listen(8080, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:8080/');
 
 }
 
