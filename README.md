0ee7942cfcb29a99868e1ae014491e627f6eba66
Richard Hitier <richard.hitier@co-libri.org>
2015-09-14 12:47:07 +0200

    rendered template by ejs

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ from openclassroom tuto
 "Des applications ultra-rapides avec Node.js"
 
 # Install
+see package.json for dependencies
 
 npm install
--- firstexpress.js
+++ firstexpress.js
@@ -1,10 +1,14 @@
 var express = require( 'express');
+var url = require('url');
 var app = express();
+
 var welcomePaths = ['/', '/index*'];
 app.get(welcomePaths, function( req, res){
     res.sendFile(__dirname+'/app/first.html');
 });
+
 app.use(function( req, res){
-    res.sendFile(__dirname+'/app/404.html');
+    res.render(__dirname+'/app/404.ejs', {wrongpath: url.parse(req.url).pathname});
 });
+
 app.listen(8080);
